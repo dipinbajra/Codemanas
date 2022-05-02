842ca17a59c63acde59fb5ce21798442d90cd963
dipinbajra <bajradipin98@gmail.com>
2022-05-02 14:38:35 +0545

    scroll to top

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,15 @@
 
-  function openResponsive(menu) {
-    menu.classList.toggle('open');
-  }
+function openResponsive(menu) {
+  menu.classList.toggle('open');
+}
+
+
+const btnScrollToTop = document.getElementById("btnScrollToTop");
+
+btnScrollToTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+});
