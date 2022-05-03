da42d39aad49a916f5f8e10b25e1a2f9ad451a96
dipinbajra <bajradipin98@gmail.com>
2022-05-03 17:26:47 +0545

    sticky menu using js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-  function openResponsive(menu) {
+// scroll to top button
+function openResponsive(menu) {
     menu.classList.toggle('open');
   }
 
@@ -12,7 +13,28 @@ btnScrollToTop.addEventListener("click", function () {
   });
 
 });
+// End scroll to top button
 
 
+//sticky menu
+jQuery(document).ready(function($) {
+  
+   var mainHeader  = $('.main-header');
+  //  var l  = mainHeader.length;
+  
+   $(window).scroll( function() {
+      // console.log( $(window).scrollTop());
+
+       var scrolled = $(window).scrollTop();
+
+      if(scrolled > 82) {
+        mainHeader.addClass('sticky');
+      } else {
+        mainHeader.removeClass('sticky');
+      }
+   });
+});
+
+//End sticky menu
 
 
